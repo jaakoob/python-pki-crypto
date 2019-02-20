2137609979b19e36cb420d655e0cc320dd1ded86
Jakob Döring <jakob.doering@s2017.tu-chemnitz.de>
2019-02-20 11:22:46 +0100

    refractor

--- generate-ca-name-constraints.py
+++ generate-ca-name-constraints.py
@@ -17,16 +17,24 @@ private_key = rsa.generate_private_key(
 public_key = private_key.public_key()
 
 certificate = x509.CertificateBuilder().subject_name(x509.Name([
-    x509.NameAttribute(NameOID.COMMON_NAME, u"My Test CA"),
-    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"MyOrg")
-])).issuer_name(x509.Name([
-    x509.NameAttribute(NameOID.COMMON_NAME, u"My Test CA")
-])).not_valid_before(datetime.datetime.now()).\
-    not_valid_after(datetime.datetime.now() + lifetime).\
-    serial_number(int(uuid.uuid4())).\
-    public_key(public_key).\
-    add_extension(x509.BasicConstraints(ca=True,
-                                        path_length=None), critical=True
+        x509.NameAttribute(NameOID.COMMON_NAME, u"My Test CA"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"MyOrg")
+    ])).issuer_name(x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u"My Test CA")
+    ])).not_valid_before(
+        datetime.datetime.now()
+    ).not_valid_after(
+        datetime.datetime.now() + lifetime
+    ).serial_number(
+        int(uuid.uuid4())
+    ).public_key(
+        public_key
+    ).add_extension(
+        x509.BasicConstraints(
+            ca=True,
+            path_length=None
+        ),
+        critical=True
     ).add_extension(
         x509.NameConstraints(
             permitted_subtrees=(
@@ -37,9 +45,11 @@ certificate = x509.CertificateBuilder().subject_name(x509.Name([
             excluded_subtrees=(),
         ),
         critical=True
-    ).sign(private_key=private_key,
-         algorithm=hashes.SHA256(),
-         backend=default_backend())
+    ).sign(
+        private_key=private_key,
+        algorithm=hashes.SHA256(),
+        backend=default_backend()
+    )
 
 with open("ca-name-restrict.pem", "wb") as f:
     f.write(private_key.private_bytes(
